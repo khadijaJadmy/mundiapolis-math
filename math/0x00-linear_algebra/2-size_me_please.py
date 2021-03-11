49cb0b5e8af682574738b1da81364903b3422463
khadija <68543421+khadijaJadmy@users.noreply.github.com>
2021-03-11 21:35:43 +0100

    Add files via upload

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,25 @@
+
+def matrix_shape(matrix):
+    val=len(matrix)
+    val2=val3=0
+    for x in matrix:
+        if(type(x)!=int) :
+            for y in matrix[matrix.index(x)]:
+                val2=len(matrix[matrix.index(x)])
+                if(type(y)!=int) :
+                    val3=len(y)
+                else:
+                    return [val,val2]
+        else:
+            return [val]
+                   
+    return [val,val2,val3]
+
+
+
+mat1 = [[1, 2], [3, 4]]
+print(matrix_shape(mat1))
+mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
+        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
+print(matrix_shape(mat2))
+
