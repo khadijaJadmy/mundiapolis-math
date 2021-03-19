e9b5f8a87eacadf1c01ec099a886102c55df3701
khadija <68543421+khadijaJadmy@users.noreply.github.com>
2021-03-19 09:34:02 +0100

    Update 0-line.py

--- math/0x01-plotting/0-line.py
+++ math/0x01-plotting/0-line.py
@@ -4,5 +4,9 @@ import matplotlib.pyplot as plt
 
 y = np.arange(0, 11) ** 3
 
-plt.plot(y)
+plt.plot(y, 'r')
+
+plt.xticks(np.arange(0, 12, 2))
+plt.yticks(np.arange(0, 1200, 200))
+plt.xlim([0, 10])
 plt.show()
